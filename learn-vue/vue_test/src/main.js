900de66c409d58acefe9f8fbf713ee057ae2013f
leesure <arm125@qq.com>
2022-07-17 23:44:47 +1000

    add components 7

--- learn-vue/vue_test/src/main.js
+++ learn-vue/vue_test/src/main.js
@@ -1,3 +1,4 @@
+// 使用这个就不能使用template属性去渲染
 import Vue from 'vue'
 import App from './App.vue'
 
@@ -6,3 +7,25 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// new Vue({
+//   el:"#app",
+//   render(createElement) {
+//     console.log(createElement);
+//     return createElement("h1","hello vue");
+//   },
+// });
+
+// new Vue({
+//   el:"#app",
+//   render: (createElement) => {
+//     return createElement(App);
+//   },
+// })
+
+
+// new Vue({
+//   el:"#app",
+//   render: createElement =>createElement(App),
+// })
+
