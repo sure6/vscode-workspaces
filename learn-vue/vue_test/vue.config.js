22fa1953496c5a590ae35e6ae2b7b8a4f03f84a7
leesure <arm125@qq.com>
2022-07-18 00:21:51 +1000

    add components 8

--- learn-vue/vue_test/vue.config.js
+++ learn-vue/vue_test/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+      // 模板来源
+      template: 'public/index.html',
+
+    },
+
+  },
+  lintOnSave: false, // 关闭语法检查
+}
