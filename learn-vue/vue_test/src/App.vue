114593ccfcd2becb0fecd3fe6fc6c0be7e56c371
leesure <arm125@qq.com>
2022-07-19 01:03:17 +1000

    add components 18

--- learn-vue/vue_test/src/App.vue
+++ learn-vue/vue_test/src/App.vue
@@ -2,9 +2,17 @@
   <div id="root">
     <div class="todo-container">
       <div class="todo-wrap">
-        <MyHeader :addTodo="addTodo"/>
-        <MyList :todos="todos" :checkTodo="checkTodo" :handleDelete="handleDelete"/>
-        <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"/>
+        <MyHeader :addTodo="addTodo" />
+        <MyList
+          :todos="todos"
+          :checkTodo="checkTodo"
+          :handleDelete="handleDelete"
+        />
+        <MyFooter
+          :todos="todos"
+          :checkAllTodo="checkAllTodo"
+          :clearAllTodo="clearAllTodo"
+        />
       </div>
     </div>
   </div>
@@ -24,36 +32,45 @@ export default {
   },
   data() {
     return {
-        todos:[
-            {id:"0001", name:"eating",done:true},
-            {id:"0002", name:"sleeping",done:false}, 
-            {id:"0003", name:"driving",done:true},
-        ]
-    }
+      todos: JSON.parse(localStorage.getItem("todos")) || [],
+      // todos:[
+      //     {id:"0001", name:"eating",done:true},
+      //     {id:"0002", name:"sleeping",done:false},
+      //     {id:"0003", name:"driving",done:true},
+      // ]
+    };
   },
   methods: {
     // 采用组件间通信, 父亲首先定义方法给儿子使用, 儿子传参数给父亲
     // add todo
-    addTodo(todo){
+    addTodo(todo) {
       // console.log("我收到了数据", todo)
       this.todos.unshift(todo);
     },
-    checkTodo(id){
-      this.todos.forEach(todo => {
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
         if (todo.id === id) todo.done = !todo.done;
       });
     },
-    handleDelete(id){
-      this.todos = this.todos.filter(todo=>todo.id !==id)
+    handleDelete(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
     },
-    checkAllTodo(done){
-      this.todos.forEach(todo => {
-        todo.done=done;
+    checkAllTodo(done) {
+      this.todos.forEach((todo) => {
+        todo.done = done;
       });
     },
-    clearAllTodo(){
-      this.todos = this.todos.filter(todo=>!todo.done)
-    }
+    clearAllTodo() {
+      this.todos = this.todos.filter((todo) => !todo.done);
+    },
+  },
+  watch: {
+    //采用本地存储, 深度监视, 当对象里面的东西改变时,也可以改
+    todos: {
+      handler(value) {
+        localStorage.setItem("todos", JSON.stringify(value));
+      },
+    },
   },
 };
 </script>
@@ -72,7 +89,8 @@ body {
   text-align: center;
   vertical-align: middle;
   cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
   border-radius: 4px;
 }
 .btn-danger {
