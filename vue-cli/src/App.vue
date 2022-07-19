d9c1101a4c7a05f3a257dd4c93b21875a31085dd
leesure <arm125@qq.com>
2022-07-20 02:34:12 +1000

    add slot 1

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <div class="container">
-      <Search/>
-      <List/>
+  <div class="container1">
+      <Category title="foods">
+          <img src="https://cn.vuejs.org/images/logo.svg">
+      </Category>
+      <Category title="games">
+      <ul>
+        <li v-for="(g, index) in games" :key="index">{{ g }}</li>
+      </ul>
+      </Category>
+      <Category title="films">
+        <img src="https://editors.dexerto.com/wp-content/uploads/2021/12/14/league-of-legends-2022-patch-schedule-all-lol-season-12-updates-changes.jpg" alt="">
+      </Category>
     </div>
 </template>
 
 <script>
-import Search from "./components/Search.vue"
-import List from "./components/List.vue"
+import Category from "./components/Category.vue"
 
 
 export default {
   name: "App",
-  components:{List,Search},
+  components:{Category},
+  data() {
+    return {
+      foods:["hot pot","barbecue","lobster","steak"],
+      games:["LOL","CF","Dota","Pubg"],
+      films:["a","b","c","d"],
+    }
+  },
 }
 </script>
 
 <style >
+.container1{
+  display: flex;
+  justify-content: space-around;
+
+}
+img{
+  width: 100%;
+}
 </style>
---
+++ vue-cli/src/components/Category.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+      <!-- 定义一个插槽  等着组件的使用者进行填充-->
+      <slot> 我是默认值,当使用者没有传递具体结构时,我会出现</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Category",
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
