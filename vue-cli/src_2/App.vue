9c9688b6491ef19d4924154d9d6ac216fc5013dd
leesure <arm125@qq.com>
2022-07-20 00:19:58 +1000

    delete extra import

---
+++ vue-cli/src_2/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="getStudents">获取学生的信息</button>
+    <button @click="getOther">获取其他信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+  name: "App",
+  methods: {
+    getStudents(){
+      axios.get("http://localhost:8080/api/H5/Test.html").then(
+        response=>{
+          console.log("请求成功了", response.data);
+        },
+        error=>{
+          console.log("请求成功了", error.message);
+        }
+      )
+    },
+    getOther(){
+      axios.get("http://localhost:8080/foo/H5/leesure.html").then(
+        response=>{
+          console.log("请求成功了", response.data);
+        },
+        error=>{
+          console.log("请求成功了", error.message);
+        }
+      )
+    }
+  },
+}
+</script>
