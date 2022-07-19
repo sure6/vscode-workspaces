381551b090971c5aaef71792794811f407e18ca0
leesure <arm125@qq.com>
2022-07-19 16:12:23 +1000

    add components 22

--- learn-vue/vue_test/src/main.js
+++ learn-vue/vue_test/src/main.js
@@ -3,6 +3,15 @@ import App from "./App";
 
 Vue.config.productionTip=false;
 
+// 假设x为全局事件代理
+// const Demo = Vue.extend({});
+// const d = new Demo();
+
+// Vue.prototype.x=d;
+
 new Vue({
+    beforeCreate(){
+        Vue.prototype.$bus=this // 安装全局事件总线
+    },
     render:h=>h(App)
 }).$mount("#app")
