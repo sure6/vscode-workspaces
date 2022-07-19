8da8e08794f513fe88e3fa25db0f44a1cc5ff6b0
leesure <arm125@qq.com>
2022-07-19 17:04:51 +1000

    add components 25

---
+++ learn-vue/vue_test/src_10/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <h1> Welcome to learn Vue!!</h1>
+    <School/>
+    <hr>
+      <!--通过父组件给子组件传递函数类型的props实现了子给父传递数据-->
+    <Student />
+
+  </div>
+</template>
+
+<script>
+import School from './components/School.vue';
+import Student from "./components/Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    Student,
+    School,
+  },
+
+};
+</script>
+
+<style>
+.main{
+  background-color: blue;
+}
+</style>
