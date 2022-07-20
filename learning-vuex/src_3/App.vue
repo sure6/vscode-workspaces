9d5c29bf0f1e518b919d9b100c5e3e19740bd2e8
leesure <arm125@qq.com>
2022-07-20 17:38:39 +1000

    learn vuex 9

---
+++ learning-vuex/src_3/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <my-count />
+  </div>
+</template>
+
+<script>
+import MyCount from './components/MyCount.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyCount
+  }
+}
+</script>
+
+
---
+++ learning-vuex/src_3/components/MyCount.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h2>current sum is :{{sum}}  </h2>
+    <!-- <h2>current sum is :{{add}}  </h2> -->
+    <h2>current sum*10 is :{{bigSum}}  </h2>
+    <h3> I learn {{subject}} in {{school}} </h3>
+    <!-- <h3> I learn {{kemu}} in {{xuexiao}} </h3> -->
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment(n)">+</button>
+    <button @click="decrement(n)">-</button>
+    <button @click="incrementOdd(n)">当前求和为奇数再加</button>
+    <button @click="incrementWait(n)">等一等再加</button>
+  </div>
+</template>
+
+<script>
+import {mapState,mapGetters,mapActions,mapMutations} from 'vuex'
+export default {
+  name: "MyCount",
+  data() { 
+    return {
+      n: 1,
+    };
+  },
+  computed:{
+    ...mapState(["sum","subject","school"]),
+    // ...mapState({add:"sum",kemu:"subject",xuexiao:"school"}),
+    ...mapGetters(["bigSum"]),
+    // ...mapGetters({bigSum:"bigSum"}),
+  },
+  methods: {
+    // increment() {
+    //   this.$store.commit('ADD',this.n);
+    // },
+    // decrement() {
+    //   this.$store.commit('SUB',this.n);
+    // },
+    ...mapMutations({increment:"ADD",decrement:"SUB"}),
+    // ...mapMutations(["ADD","SUB"]),
+  
+    // incrementOdd() {
+    //   this.$store.dispatch('addOdd',this.n);
+    // },
+    // incrementWait() {
+    //   this.$store.dispatch('addWait',this.n);
+    // },
+    ...mapActions({incrementOdd:"addOdd",incrementWait:"addWait"}),
+    // ...mapActions(["addOdd","addWait"])
+  },
+};
+</script>
+
+
