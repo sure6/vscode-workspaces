52f9a7fd93cd255c9205cdb84881d7aac7425b1d
leesure <arm125@qq.com>
2022-07-20 18:17:30 +1000

    vue_test mobile

---
+++ learning-vuex/src_4/components/MyPerson.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Person List</h1>
+    <input type="text" placeholder="please input name" v-model="name" />
+    <button @click="add">add</button>
+    <ul>
+      <li v-for="(p, index) in personList" :key="index">{{ p.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+import {nanoid} from "nanoid"
+export default {
+  name: "MyPerson",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  computed: {
+    ...mapState(["personList"]),
+  },
+  methods: {
+    add() {
+        const personObj={id:nanoid(), name:this.name}
+        this.$store.commit('ADD_PERSON',personObj)
+        this.name=""
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ learning-vuex/src_4/store/index.js
@@ -0,0 +1,64 @@
+//in  this file, use Vuex to create core store
+
+//import Vuex
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex)
+// prepare actions---use action in response components
+const actions={
+    // add:function(context,value){
+    //     console.log('add is invoked in actions',context ,value);
+    //     context.commit('ADD',value)
+    // },
+    // sub(context,value){
+    //     context.commit('SUB',value)
+    // },
+    addOdd(context,value){
+        if(context.state.sum % 2){
+            context.commit('ADD',value)
+        }
+    },
+    addWait(context,value){
+        setTimeout(()=>{
+            context.commit('ADD',value)
+        },500)
+    }
+}
+// prepare mutations---use to operate data  (state)
+const mutations ={
+    ADD(state,value){
+        console.log('add is invoked in mutations',state ,value);
+        state.sum+=value
+    },
+    SUB(state,value){
+        state.sum-=value
+    },
+    ADD_PERSON(state,value){
+        state.personList.unshift(value)
+    }
+}
+// prepare state---use to store data 
+const state ={
+    sum: 0,
+    school:"uow",
+    subject:"CSCI927",
+    personList:[
+        {id:"001",name:"Jerry"}
+    ]
+}
+// prepare getters---use to process state
+const getters={
+    bigSum(state){
+        return state.sum*10
+    }
+}
+
+//create store
+const store =new Vuex.Store({
+    actions,
+    mutations,
+    state,
+    getters
+})
+
+export default store;
