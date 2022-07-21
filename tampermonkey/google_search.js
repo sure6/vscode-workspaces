d4bd6a8bf8fb930ef7a0cd475a29ff89ef07fee0
leesure <arm125@qq.com>
2022-07-21 23:00:20 +1000

    tampermonkey_script_google_search 2

--- tampermonkey/google_search.js
+++ tampermonkey/google_search.js
@@ -1,6 +1,6 @@
+
 (function () {
     'use strict';
-   
     const images = [
         `https://variety.com/wp-content/uploads/2019/02/pubg-mobile-1.png`,
         `https://cdn.mos.cms.futurecdn.net/Tqq3BVZZAkbUyGvZmSfjQW.jpg`,
@@ -8,96 +8,98 @@
         `https://www.riotgames.com/darkroom/1440/08bcc251757a1f64e30e0d7e8c513d35:be16374e056f8268996ef96555c7a113/wr-cb1-announcementarticle-banner-1920x1080.png`,
         `https://coolhdwall.com/storage/202101/pubg-frying-pan-playerunknowns-battlegrounds-4K-wallpaper-pc-preview.jpg`,
         `https://www.avenga.com/wp-content/uploads/2021/09/dota-2-header.jpeg`,
-
     ]
+   
+   
+    let isGoogle = location.origin == 'https://www.google.com';
 
-    // Your code here...
-    var google = window.location.origin == 'https://www.google.com'
-
-
-    //change button and background image in google search home page
-    if (document.getElementsByClassName('gNO89b')[1] && google) {
-        document.getElementsByClassName('gNO89b')[1].style.backgroundColor = 'PaleGreen';
+     //change button  in google search home page
+     function changeButtonStyle(selector,num,styleObj){
+        if ($(selector).get(num) && isGoogle) {
+            $(selector).eq(num).css(styleObj);
+        }
     }
-    if (document.getElementsByClassName('RNmpXc')[1] && google) {
-        document.getElementsByClassName('RNmpXc')[1].style.backgroundColor = 'LightSkyBlue';
-    }
-    if (google && window.location.pathname == '/') {
 
-        // document.body.style.background = 'url("' + images[Math.floor(Math.random() * images.length)] + '") no-repeat center 100%';
-        $("body").css({
-            "width":"100%",
-            "height":"100%",
-            "background-image": "url(" + images[Math.floor(Math.random() * images.length)] + ")",
-            "background-repeat": "no-repeat",
-            "background-size": "100% 100%"
-        })
-      
 
+    //change background image in google search home page
+    function changeBodyBackgrond(selector="body"){
+
+        let imgIndex=Math.floor(Math.random() * images.length);
+
+        if (isGoogle && location.pathname == '/') {
+            $(selector).css({
+                "width":"100%",
+                "height":"100%",
+                "background-image": "url(" + images[imgIndex] + ")",
+                "background-repeat": "no-repeat",
+                "background-size": "100% 100%"
+            })
+        }
+    
     }
+
+    changeButtonStyle('.gNO89b',1,{"background-color":"palegreen"});
+    changeButtonStyle('.RNmpXc',1,{"background-color":"LightSkyBlue"});
+    changeBodyBackgrond();
+    
 
     // hightlight tap control for google search
-    if (document.getElementsByClassName('hdtb-mitem') && google) {
+    if ($('hdtb-mitem') && isGoogle) {
 
-        var listBtn = document.getElementsByClassName('hdtb-mitem');
+        let $listBtn = $('.hdtb-mitem');
 
-        for (var i = 0; i < 6; i++) {
+        $.each($listBtn,function(i, value){
             if (i == 0) {
-                listBtn[i].onmouseover = function () {
-                    this.style.backgroundColor = 'Aqua';
-                }
-                listBtn[i].onmouseout = function () {
-                    this.style.backgroundColor = null;
-                }
-                continue;
+                $(value).on("mouseover", function () {
+                    $(this).css({"background-color":"Aqua"})
+                })
+                $(value).on("mouseout",function () {
+                    $(this).css({"background-color":""})
+                })
+                return;
             }
             if (i == 5) {
-                document.getElementsByClassName('GOE98c')[0].onmouseover = function () {
-                    this.style.backgroundColor = 'Aqua';
-                }
-                document.getElementsByClassName('GOE98c')[0].onmouseout = function () {
-                    this.style.backgroundColor = null;
-                }
-                continue;
+                $('.GOE98c').first().on("mouseover", function () {
+                    $(this).css({"background-color":"Aqua"})
+                })
+                $('.GOE98c').first().on("mouseout", function () {
+                    $(this).css({"background-color":""})
+                })
+                return;
             }
-            listBtn[i].childNodes[0].onmouseover = function () {
-                this.style.backgroundColor = 'Aqua';
-            }
-            listBtn[i].childNodes[0].onmouseout = function () {
-                this.style.backgroundColor = null;
-            }
-        }
+            $(value).children().first().on("mouseover", function () {
+                $(this).css({"background-color":"Aqua"})
+            })
+            $(value).children().first().on("mouseout", function () {
+                $(this).css({"background-color":""})
+            })
+        })
 
     }
 
     // change google logo to uow logo
-    if (google) {
-        document.querySelector('.logo a img').setAttribute('src', 'https://www.uow.edu.au/assets/uow-channel/public/site-assets/images/logo/logo.svg');
-        document.querySelector('.logo a img').setAttribute('height', 80);
-        document.querySelector('.logo a').setAttribute('taget', 80);
+    if (isGoogle) {
+        $('.logo a img').attr({src: 'https://www.uow.edu.au/assets/uow-channel/public/site-assets/images/logo/logo.svg',height:80});
+        $('.logo a').attr('taget', 80);
     }
 
     // highlight content list searched
-    var contenlist = document.querySelectorAll('.LC20lb');
-    if (google && contenlist.length) {
-
-        for (var k = 0; k < contenlist.length; k++) {
-            contenlist[k].onmouseover = function () {
-                this.style.backgroundColor = 'SkyBlue';
-            }
-            contenlist[k].onmouseout = function () {
-                this.style.backgroundColor = null;
-            }
-
-        }
-
-
+    let $contenlist = $('.LC20lb');
+    if (isGoogle && $contenlist) {
+        
+        $.each($contenlist,function (i,value) { 
+            $(value).on("mouseover",function(){
+                $(this).css({"background-color":"SkyBlue"});
+            })
+            $(value).on("mouseout",function(){
+                $(this).css({"background-color":""});
+            })
+        })
+       
         // add target='_blank' for a tags in google
-        var allAtags = document.querySelectorAll('a');
-        for (var x = 0; x < allAtags.length; x++) {
-
-            allAtags[x].setAttribute('target', '_blank');
-        }
+        $('a').each(function (i,value) {
+            $(value).attr({target:'_blank'})
+        })
 
     }
 })();
