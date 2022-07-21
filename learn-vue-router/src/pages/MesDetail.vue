b65bb2ef03e2cfa7346e1724b6f6dbc5564f2202
leesure <arm125@qq.com>
2022-07-21 13:32:19 +1000

    learn vue-router 7

--- learn-vue-router/src/pages/MesDetail.vue
+++ learn-vue-router/src/pages/MesDetail.vue
@@ -2,17 +2,21 @@
   <ul>
     <!-- <li>消息编号{{$route.query.id}}</li>
     <li>消息列表{{$route.query.title}}</li> -->
-    <li>消息编号{{$route.params.id}}</li>
-    <li>消息列表{{$route.params.title}}</li>
+    <!-- <li>消息编号{{$route.params.id}}</li>
+    <li>消息列表{{$route.params.title}}</li> -->
+    <li>消息编号:{{id}}</li>
+    <li>消息列表:{{title}}</li>
   </ul>
+ 
 </template>
 
 <script>
 export default {    
     name:"MesDetail",
-    mounted(){
-        console.log(this.$route);
-    }
+    props:["id","title"],
+    // mounted(){
+    //     console.log(this.$route);
+    // }
 }
 </script>
 
--- learn-vue-router/src/router/index.js
+++ learn-vue-router/src/router/index.js
@@ -29,7 +29,18 @@ const router = new VueRouter({
                         {
                             name:"xiangqin",
                             path:"detail/:id/:title",
-                            component:MesDetail
+                            component:MesDetail,
+                            // prop第一种写法, 值为对象, 该对象中所用key-value都是以props形式传给detail
+                            // props:{a:1, b:"hello"}
+                            // props的第二种写法,值为布尔值,若布尔值为真, 就会把路由组件收到的所有params参数,以props的形式传给Detail组件
+                            // props:true,
+                            // props的第三种写法, 值为函数
+                            props($route){
+                                return {
+                                    id:$route.query.id,
+                                    title:$route.query.title,
+                                }
+                            }
                         }
                     ]
                 }
