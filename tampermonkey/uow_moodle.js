5ddb71f45faa1283ba795626289482c8b8e49339
leesure <arm125@qq.com>
2022-07-23 13:49:34 +1000

    optimize tampermonkey script

--- tampermonkey/uow_moodle.js
+++ tampermonkey/uow_moodle.js
@@ -35,3 +35,41 @@
     }
 
 })();
+
+/*
+(function() {
+    'use strict';
+
+    // definite opearation by mouse event
+    function opearation(liitems){
+        for(var i=0; i<liitems.length; i++){
+            liitems[i].onmouseover=function(){
+                this.style.backgroundColor='SkyBlue';
+            }
+            liitems[i].onmouseout=function(){
+                this.style.backgroundColor=null;
+            }
+        }
+    }
+    // judge window .origin
+    var isMoodle=window.origin=='https://moodle.uowplatform.edu.au';
+    // highlight all item
+    var courseList=document.querySelectorAll('.courseovbox');
+    var dropdownlist= document.querySelectorAll('.dropdown-item');
+    var usertext=document.querySelector('.usertext')
+    if(isMoodle){
+        opearation(courseList);
+        opearation(dropdownlist);
+
+        // modity text style
+        usertext.style.color='red';
+        usertext.style.fontWeight='1000';
+
+        //add bias name
+        var spanBais = document.createElement('span');
+        spanBais.setAttribute('style','color:red;font-weight:1000;');
+        spanBais.innerText='(leesure)';
+        usertext.after(spanBais);
+    }
+})();
+*/
