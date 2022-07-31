d47e782926e5864e93b8282b307ec2fa7961b1da
sure6 <31686128+sure6@users.noreply.github.com>
2022-07-31 11:47:19 +1000

    Update google_search.js

--- tampermonkey/google_search.js
+++ tampermonkey/google_search.js
@@ -138,6 +138,8 @@
 
     addMoodleCourse("moodle","https://moodle.uowplatform.edu.au/my/")
     addMoodleCourse("UOW library","https://www.uow.edu.au/library/")
+    addMoodleCourse("Zulipchat","https://uow-csit998-22-23.zulipchat.com/#")
+    
     addMoodleCourse("timetable","https://solss.uow.edu.au/sid/sols_tutorial_enrolment.my_timetable?P_STUDENT_NUMBER=6732409&P_SESSION_ID=&p_cs=24189084001212943044&")
     addMoodleCourse("CSIT985","https://moodle.uowplatform.edu.au/course/view.php?id=33234")
     addMoodleCourse("CSCI935","https://moodle.uowplatform.edu.au/course/view.php?id=33193")
