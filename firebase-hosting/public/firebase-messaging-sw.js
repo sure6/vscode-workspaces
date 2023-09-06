c80b9fe0d8fab2d0d900f00da5a2b42a8d5e1713
leesure <xluow5965@gmail.com>
2023-09-06 17:09:16 +1000

    add node env

--- firebase-hosting/public/firebase-messaging-sw.js
+++ firebase-hosting/public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
- importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
-
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+// import { getMessaging,onBackgroundMessage  } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging-sw.js";
   // Initialize Firebase
   const firebaseConfig = {
     apiKey: "AIzaSyB3jgj1BmeLLtET6UETMMqAIWttfKv013s",
@@ -12,17 +13,31 @@ importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
     measurementId: "G-CV402Q1KB3"
   };
   
-
+  // const messaging = getMessaging(firebaseConfig);
   firebase.initializeApp(firebaseConfig);
   const messaging = firebase.messaging();
 
-  messaging.onBackgroundMessage(function(payload) {
-    console.log("Received background message ", payload);
- 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-    };
- 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+
+
+  messaging. onBackgroundMessage(payload => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    // const notificationTitle = 'Background Message Title';
+    // const notificationOptions = {
+    //   body: 'Background Message body.',
+    // };
+  
+    // self.registration.showNotification(notificationTitle,
+    //   notificationOptions);
   });
+  
+  // messaging.onBackgroundMessage((payload)=> {
+  //   console.log("Received background message ", payload);
+ 
+  //   const notificationTitle = payload.notification.title;
+  //   const notificationOptions = {
+  //     body: payload.notification.body,
+  //   };
+ 
+  //   self.registration.showNotification(notificationTitle, notificationOptions);
+  // });
